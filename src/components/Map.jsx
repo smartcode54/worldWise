90cce621cc25a1afc6d2f7b586399cf3b01ea137
smartcode54@gmail.com <smartcode54@gmail.com>
2025-12-14 00:56:52 +0700

    feat: add react-leaflet map library

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,21 +1,41 @@
 
+import { useMemo } from 'react';
 import styles from './Map.module.css';
-import {useNavigate, useSearchParams} from 'react-router-dom';
-
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function Map() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
-  
+
+  const mapPosition = useMemo(() => {
+    if (lat && lng) {
+      return [Number(lat), Number(lng)];
+    }
+    return [13.842849, 100.510114];
+  }, [lat, lng]);
+
   return (
     <div className={styles.mapContainer} onClick={() => navigate('/app/form')}>
-        
-     <h1>Map</h1>
-     <h3>Position</h3>
-     <p>Latitude: {lat || 'Select on list'}</p>
-     <p>Longitude: {lng || 'Select on list'}</p>
+
+      <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> smartCode54'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+
+      <h1>Map</h1>
+      <h3>Position</h3>
+      <p>Latitude: {lat || 'Select on list'}</p>
+      <p>Longitude: {lng || 'Select on list'}</p>
     </div>
   )
 }
